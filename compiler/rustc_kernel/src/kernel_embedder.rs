eeb2cc262b0e00a60de69d76c7b60544e2c3c6f1
Niek Aukes <n.aukes@student.utwente.nl>
2024-05-13 21:04:59 +0200

    Add embed_kernel function

--- compiler/rustc_kernel/src/kernel_embedder.rs
+++ compiler/rustc_kernel/src/kernel_embedder.rs
@@ -1,19 +1,19 @@
-use rustc_middle::ty::{ConstData, TyCtxt, TyKind};
-use rustc_middle::mir::Place;
+use rustc_middle::ty::{Const as TyConst, ConstData, ConstKind, TyCtxt, TyKind, ValTree};
+use rustc_middle::mir::Const;
 
-fn literal_place_builder<'tcx>(tcx: TyCtxt<'tcx>, code: &[u8]) -> Place<'tcx> {
+fn literal_place_builder<'tcx>(tcx: TyCtxt<'tcx>, code: &[u8]) -> Const<'tcx> {
     // we need to create a new literal place builder
     // that will be used to create the new literal place
     // that will hold the code
     
-    let tykind =  TyKind::Array(tcx.types.u8, code.len() as u64);
-    let const_kind = ConstKind::Value(ConstValue::Slice(code));
-    /*let x = ConstData {
-        ty: tcx.types.
-        val: ConstValue::Slice(code),
-        has_value: true,
-    }*/
-    todo!()
+    let tykind =  TyKind::Array(tcx.types.u8, TyConst::from_target_usize(tcx, code.len() as u64));
+    let ty = tcx.mk_ty_from_kind(tykind);
+    let valtree = ValTree::from_raw_bytes(tcx, code);
+    let const_kind = ConstKind::Value(valtree);
+
+    let x = tcx.mk_ct_from_kind(const_kind, ty);
+
+    Const::from_ty_const(x, tcx)
 }
 
 
--- compiler/rustc_kernel/src/lib.rs
+++ compiler/rustc_kernel/src/lib.rs
@@ -10,7 +10,7 @@ pub fn provide(providers: &mut Providers) {
     providers.compile_kernel_module = |tcx, module| {
         //let mut codegen_backend = tcx.codegen_backend();
         //codegen_backend.compile_kernel_module(tcx, module)
-        let _ = kernel_embedder::embed_kernel(tcx, module);
+        let _ = kernel_embedder::embed_kernel(tcx, &[]);
         // tcx.arena.alloc
         todo!()
     };
